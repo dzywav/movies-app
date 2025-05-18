e1459ad7d8109ca2cfcd52138a985e9ba6860827
Daniel Rosado <88495712+dzywav@users.noreply.github.com>
2025-05-18 15:10:55 -0600

    Big two
    Se creó el módulo components junto con dos nuevos componentes, los slideshows para los posters y para los backdrops.

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -6,6 +6,7 @@ import { Tab1Page } from './tab1.page';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 import { PipesModule } from '../pipes/pipes.module';
+import { ComponentsModule } from "../components/components.module";
 
 @NgModule({
   imports: [
@@ -14,7 +15,9 @@ import { PipesModule } from '../pipes/pipes.module';
     FormsModule,
     PipesModule,
     Tab1PageRoutingModule,
-  ],
+    CommonModule,
+    ComponentsModule
+],
   declarations: [Tab1Page],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
